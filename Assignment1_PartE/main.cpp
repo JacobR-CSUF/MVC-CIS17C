fa18e6f7bb892a01996faaded02512eb82683f49
JacobR-CSUF <157658230+JacobR-CSUF@users.noreply.github.com>
2025-07-20 17:14:14 -0700

    added solution main.cpp
    Populated the file with the solution for the A1PE ReadME.md

--- Assignment1_PartE/main.cpp
+++ Assignment1_PartE/main.cpp
@@ -1,1 +1,34 @@
+// Jacob Rodas
+// CIS-17C
+// Assignment #1, Part E
 
+#include <iostream>
+using namespace std;
+
+void deleteS(char *);
+
+int main() {
+  char s[12] = "Mississippi";
+
+  deleteS(s);
+
+  cout << s << endl; // Prints "Miiippi"
+
+  return 0;
+}
+
+void deleteS(char *str) { // Will use pre-declared var. to read string.
+  char *writeStr = str;   // Additional var. of ptr type to rewrite string.
+
+  /* Checks if 's' and 'S' are present per char in a string.
+   * Rewrites string without those characters
+   * using writeStr to track new positions. */
+  while (*str != '\0') {
+    if (*str != 's' && *str != 'S') {
+      *writeStr = *str;
+      writeStr++;
+    }
+    str++;
+  }
+  *writeStr = '\0'; // Null terminate string.
+}
